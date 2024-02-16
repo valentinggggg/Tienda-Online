a71614d5085d23860c000213872b60237db4d7e2
valentinggggg <garellovalentin@gmail.com>
2024-02-16 19:39:59 -0300

    Update Home.jsx

--- Proyecto/src/components/Home.jsx
+++ Proyecto/src/components/Home.jsx
@@ -1,9 +1,11 @@
 function Home(){
     return(
         <main>
-           <section className="Inicio bg-red-400 w-screen min-h-screen ml-28">
-                <article className="Ofertas/Carrousell">
-
+           <section className="Inicio bg-red-400 w-screen min-h-screen">
+                <article className="Ofertas/Carrousell flex justify-center items-center">
+                    <div className="Carrousell bg-green-300 lg:w-[1200px] lg:h-[400px] mt-4">
+                
+                    </div>
                 </article>
                 <article className="ProductosTendencia">
                         
